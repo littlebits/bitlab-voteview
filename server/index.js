156515dc1735de912d563688f2a470279bd45f9c
Jason Kuhrt <jasonkuhrt@me.com>
2015-01-23 00:23:48 -0500

    Move boot log before gluding things together; Otherwise the boot logging gets lost in application logs

--- server/index.js
+++ server/index.js
@@ -5,12 +5,12 @@ var log = require('./log')
 
 
 
-Glue(data.deviceProjectMappings)
-
-
-
 log.info('=======================')
 log.info('Booting bitLab Voteview')
 log.info('=======================')
 log.info('version: %j', version)
 log.info('-----------------------')
+
+
+
+Glue(data.deviceProjectMappings)
