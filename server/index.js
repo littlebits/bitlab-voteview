7386ede9d441cf4ea8ad33bc7e5250953e65bf56
Jason Kuhrt <jasonkuhrt@me.com>
2015-01-22 23:53:35 -0500

    On boot log a banner stating the version

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
+var version = require('./package').version
 var data = require('./data')
 var Glue = require('./glue')
+var log = require('./log')
 
 
 
 Glue(data.deviceProjectMappings)
+
+
+
+log.info('=======================')
+log.info('Booting bitLab Voteview')
+log.info('=======================')
+log.info('version: %j', version)
+log.info('-----------------------')
